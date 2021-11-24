d1b3cb2e36e6164f6c127f728d477be866807cfa
M1075159ganesh <Ganesh.P@mindtree.com>
2021-11-24 10:40:59 +0000

    quiz end 1

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -4,7 +4,7 @@ class QuizEnd extends Component{
         return(
         <div>
             <p>Thanks for playing!</p>
-            <a href='#'>Reset Quiz</a>
+            <a href=''>Reset Quiz</a>
         </div>
         )
     }
